9a859549b4c1f8e413ac8da707eb37129c16e145
Wh0 <87895460+Shodydosh@users.noreply.github.com>
2022-05-26 23:07:34 +0700

    Create missing-number.cpp

---
+++ missing-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] != i) return nums[i]-1;
+        }
+        return nums[nums.size()-1]+1;
+    }
+};
