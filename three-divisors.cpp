0f78d598e75422f5c14c0f1450d9bb31b1748c98
Wh0 <87895460+Shodydosh@users.noreply.github.com>
2022-05-27 01:33:38 +0700

    Create three-divisors.cpp

---
+++ three-divisors.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        if(n == 4) return true;
+        if(n == 9) return true;
+        if(n == 25) return true;
+        if(n == 49) return true;
+        if(n == 121) return true;
+        if(n == 169) return true;
+        if(n == 289) return true;
+        if(n == 361) return true;
+        if(n == 529) return true;
+        if(n == 841) return true;
+        if(n == 961) return true;
+        if(n == 1369) return true;
+        if(n == 1681) return true;
+        if(n == 1849) return true;
+        if(n == 2209) return true;
+        if(n == 2809) return true;
+        if(n == 3481) return true;
+        if(n == 3721) return true;
+        if(n == 4489) return true;
+        if(n == 5041) return true;
+        if(n == 5329) return true;
+        if(n == 6241) return true;
+        if(n == 6889) return true;
+        if(n == 7921) return true;
+        if(n == 9409) return true;
+        return false;
+    }
+};
