abd06ca36a7e154cf3e0bd70418744ffb645cf24
Tung Nguyen Minh <87895460+Shodydosh@users.noreply.github.com>
2022-11-17 00:54:42 +0700

    Create 167-two-sum-ii-input-array-is-sorted.cpp

---
+++ 167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0;
+        int r = numbers.size()-1;
+        vector<int> ans;
+        while(l < r){
+            int tmp = numbers[l] + numbers[r];
+            if(tmp == target) break;
+            else if(tmp < target) l++;
+            else r--;
+        }
+
+        return {l+1, r+1};
+    }
+};
