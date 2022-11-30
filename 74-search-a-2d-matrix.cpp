3d1827f900c1d41d5aa7c87ac3f43f93c2407c84
Tung Nguyen Minh <87895460+Shodydosh@users.noreply.github.com>
2022-11-30 14:24:06 +0700

    Create 74-search-a-2d-matrix.cpp

---
+++ 74-search-a-2d-matrix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+       for(int i=0; i<matrix.size(); i++){
+           if(matrix[i][0] <= target && matrix[i][matrix[i].size()-1] >= target){
+               for(int j = 0; j<matrix[i].size(); j++){
+                   if(matrix[i][j] == target) return true;
+               }
+           }
+       } 
+       return false;
+    }
+};
